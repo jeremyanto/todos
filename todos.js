6125a06f8dcb3b1a77229a608ac72619442f018f
Jérémy Antonini <55101929+jeremyanto@users.noreply.github.com>
2020-03-01 19:47:12 +0100

    second commit

--- todos.js
+++ todos.js
@@ -3,6 +3,10 @@ new Vue({
     data: {
         newTodo: "",
         todoList: [],
+        //completed: false,
+        filter: 'all',
+        editing: null,
+        oldTodo: ""
     },
     methods: {
         addTodo: function () {
@@ -14,7 +18,74 @@ new Vue({
                 title: value,
             })
             this.newTodo = ""
-        }
+
+        },
+        deleteTodo(todo) {
+            this.todoList = this.todoList.filter(i => i !== todo)
+        },
+
+        deleteCompleted(){
+            this.todoList = this.todoList.filter(todo => !todo.completed)
+        },
+
+        editTodo(todo){
+            this.editing = todo
+            this.oldTodo = todo.name
+        },
+
+        doneEdit(todo){
+            this.editing = null
+        },
+
+        cancelEdit(){
+            this.editing.name = this.oldTodo
+            this.doneEdit()
+        },
     },
 
+    computed: {
+
+        allDone: {
+            get() {
+                return this.remaining === 0
+            },
+            set(value) {
+                
+                    this.todoList.forEach(todo => {
+                        todo.completed = value
+                    })
+                
+            }
+        },
+
+        remaining() {
+           return this.todoList.filter(todo => !todo.completed).length
+        },
+
+        completed() {
+            return this.todoList.filter(todo => todo.completed).length
+         },
+
+        filteredTodos() {
+            if(this.filter === 'todo'){
+                return this.todoList.filter(todo => !todo.completed)
+            } else if(this.filter === 'done'){
+                return this.todoList.filter(todo => todo.completed)
+            }
+            return this.todoList
+        }
+
+
+    },
+    directives: {
+        focus (el, value){
+            if (value){
+                el.focus()
+            }
+        }
+    }   
+
 })
+
+
+  
